978a388797ddff9ea5faba6236b44b2d7c0d44ad
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-08 19:55:49 +0530

    feat: update routes

--- projects/ch-web/src/app/app-routing.module.ts
+++ projects/ch-web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  
+  { path: 'dashboard', component: DashBoardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
