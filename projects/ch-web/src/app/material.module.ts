f0404596ce5f60bd9d1bb68ca873714d284615f1
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-08 23:32:25 +0530

    feat: implement search by pin

--- projects/ch-web/src/app/material.module.ts
+++ projects/ch-web/src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -15,11 +17,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     MatAutocompleteModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatNativeDateModule,
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule
@@ -27,11 +31,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatAutocompleteModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatNativeDateModule,
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule
--- projects/ch-web/src/app/use-pin/use-pin.component.ts
+++ projects/ch-web/src/app/use-pin/use-pin.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { CowinDataService } from 'projects/ch-web/src/app/cowin-data.service';
+
+export interface SearchByPinData {
+  pin: string;
+  date: Date;
+  isForWeek: boolean
+}
 
 @Component({
   selector: 'ch-use-pin',
@@ -7,9 +16,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsePinComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: CowinDataService
+  ) {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    this.minDate = currentDate;
+    this.maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDay + 7);
+  }
+
+  searchForm: FormGroup = this.formBuilder.group({
+    pin: ['', [Validators.required, Validators.pattern('^[1-9][0-9]{5}$')]],
+    date: ['', [Validators.required]],
+    isForWeek: ['']
+  });
+  minDate: Date;
+  maxDate: Date;
 
   ngOnInit(): void {
+    this.searchForm.get('date').setValue(this.minDate);
+  }
+
+  onSubmit() {
+    const searchData: SearchByPinData = this.searchForm.value;
+    const dateString: string = `${searchData.date.getDate()}-${searchData.date.getMonth() + 1}-${searchData.date.getFullYear()}`
+    if (searchData.isForWeek) {
+      this.dataService.searchAvailabilityByPinForWeek(searchData.pin, dateString).subscribe((res) => console.log(res));
+    } else {
+      this.dataService.searchAvailabilityByPin(searchData.pin, dateString).subscribe((res) => console.log(res));
+    }
   }
 
 }
