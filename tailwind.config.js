6c1dcc7dcf60df92ad2570c1e69c905523ac3ef4
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-08 23:54:29 +0530

    feat: add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['.projects/ch-web/src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')
+    , require('@tailwindcss/line-clamp')
+    , require('@tailwindcss/typography')
+  ],
+};
