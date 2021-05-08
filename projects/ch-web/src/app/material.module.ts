77978db4741002084a1db80d7bbcc341a6564f67
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-08 22:11:19 +0530

    feat: add tab for search

--- projects/ch-web/src/app/material.module.ts
+++ projects/ch-web/src/app/material.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -9,16 +13,24 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [],
   imports: [
+    MatAutocompleteModule,
     MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule
   ],
   exports: [
+    MatAutocompleteModule,
     MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatSidenavModule,
     MatTabsModule,
--- projects/ch-web/src/app/search-availability/search-availability.component.ts
+++ projects/ch-web/src/app/search-availability/search-availability.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuOption } from '../models/menu-option';
 
 @Component({
   selector: 'ch-search-availability',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-availability.component.scss']
 })
 export class SearchAvailabilityComponent implements OnInit {
+
+  links: MenuOption[] = [
+    { icon: '', label: 'By Pin', link: '/search/pin'},
+    { icon: '', label: 'By District', link: '/search/district'},
+  ];
+  activeLink: string = this.links[0].link;
 
   constructor() { }
 
