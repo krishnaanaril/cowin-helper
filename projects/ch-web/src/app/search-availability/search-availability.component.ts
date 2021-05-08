65f7f6df81cd417061d87e73d064443227ce9ca0
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-08 20:55:03 +0530

    feat: add search availability component

---
+++ projects/ch-web/src/app/search-availability/search-availability.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ch-search-availability',
+  templateUrl: './search-availability.component.html',
+  styleUrls: ['./search-availability.component.scss']
+})
+export class SearchAvailabilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
