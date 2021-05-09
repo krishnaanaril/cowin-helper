3781e97bc2a7437a0ec45bdf7c85dff88856e110
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-09 23:25:42 +0530

    feat: add service worker support

--- projects/ch-web/src/app/app.module.ts
+++ projects/ch-web/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpProgressInterceptor } from './http-progress.interceptor';
 import { DayCenterInfoComponent } from './day-center-info/day-center-info.component';
 import { WeekCenterInfoComponent } from './week-center-info/week-center-info.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -39,7 +41,13 @@ import { WeekCenterInfoComponent } from './week-center-info/week-center-info.com
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpProgressInterceptor, multi: true },
