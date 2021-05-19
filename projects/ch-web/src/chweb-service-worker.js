dddd329496758e7b8a2a3f412a65915251dfada1
krishnaanaril <krishnamohan.a.m@gmail.com>
2021-05-19 23:04:36 +0530

    feat: add periodic sync support

--- projects/ch-web/src/chweb-service-worker.js
+++ projects/ch-web/src/chweb-service-worker.js
@@ -1,7 +1,58 @@
 importScripts('./ngsw-worker.js');
+importScripts('./utilities.js');
 
 (function () {
-    'use strict';      
+    'use strict';
+
+    navigator.permissions.query({
+        name: 'periodic-background-sync',
+    }).then((status) => {
+        if (status.state === 'granted') {
+            // Periodic background sync can be used.            
+            if ('periodicSync' in self.registration) {
+                try {
+                    self.registration.periodicSync.register('content-sync', {
+                        // An interval of 5 min.
+                        minInterval: 5 * 60 * 1000,
+                    }).then(() => {
+                        set('periodicSyncSupport', true);
+                    });
+                } catch (error) {
+                    // Periodic background sync cannot be used.
+                    set('periodicSyncSupport', false);
+                }
+            } else {
+                set('periodicSyncSupport', false);
+            }
+            // console.log('Yes');
+        } else {
+            // Periodic background sync cannot be used.
+            set('periodicSyncSupport', false);
+            // console.log('NO');
+        }
+    });
+
+    self.addEventListener('periodicsync', (event) => {
+        if (event.tag === 'content-sync') {
+          // See the "Think before you sync" section for
+          // checks you could perform before syncing.
+          event.waitUntil(syncContent());
+        }
+        // Other logic for different tags as needed.
+      });
+
+    self.addEventListener('message', (event) => {
+        if (event.data && event.data.type === 'FROM_APP') {
+            console.log('message received from app');
+            self.registration.showNotification('message received from app');
+            self.clients.matchAll().then((clients) => {
+                if (clients && clients.length) {
+                    //Respond to last focused tab
+                    clients[0].postMessage({ type: 'FROM_SW' });
+                }
+            });
+        }
+    })
 
     self.addEventListener('notificationclick', (event) => {
         console.log("This is custom service worker notificationclick method.");
@@ -10,5 +61,6 @@ importScripts('./ngsw-worker.js');
         if (clients.openWindow && event.notification.data.url) {
             event.waitUntil(clients.openWindow(event.notification.data.url));
         }
-    });}
-());
+    });
+}
+    ());
