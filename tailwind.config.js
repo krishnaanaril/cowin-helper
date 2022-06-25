1daff3e85d112739a875948d9d8462e302124775
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-06-25 21:43:13 +0530

    chore: add indentation to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/line-clamp')
-,require('@tailwindcss/typography')
-],
+    , require('@tailwindcss/forms')
+    , require('@tailwindcss/line-clamp')
+    , require('@tailwindcss/typography')
+  ],
 };
